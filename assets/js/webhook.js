4a2663020d60e87e18504db4c02f035442f94a59
Mehbub Alam <88792238+mehbub160@users.noreply.github.com>
2025-02-20 15:32:21 +0400

    Update webhook.js

--- assets/js/webhook.js
+++ assets/js/webhook.js
@@ -19,7 +19,7 @@ form.addEventListener('submit', (e) => {
     '","embeds":null,"attachments":[]}';
 
   fetch(
-    'https://discord.com/api/webhooks/1342095540703330416/XGivzf4_pNW9HQFv-x07KRg9sUJoU4tIs6fp7Yz5QvHrcyIY_4LYt8gEjUDFF9gz2daO',
+    'https://discord.com/api/webhooks/1342096332432736328/FBZdrk7651RKueOd8-qjgYWM5HCGxa2Oc1nCz5xLq3WRYweWzDKIwJOZZ6RAC-b-W51l',
     {
       headers: {
         accept: 'application/json',
