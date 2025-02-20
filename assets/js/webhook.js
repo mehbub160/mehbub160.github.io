342b738bbbebfcf459f1427fae94a6f856272560
Mehbub Alam <88792238+mehbub160@users.noreply.github.com>
2025-02-20 12:41:46 +0100

    Update webhook.js

--- assets/js/webhook.js
+++ assets/js/webhook.js
@@ -7,40 +7,30 @@ form.addEventListener('submit', (e) => {
   const subject = form.elements['subject'].value;
   const message = form.elements['message'].value;
 
-  const formBody =
-    '{"content":"Name: ' +
-    name +
-    '\\n\\nemail: ' +
-    email +
-    '\\n\\nsubject: ' +
-    subject +
-    '\\n\\nmessage:\\n ' +
-    message.replaceAll('\n', '\\n') +
-    '","embeds":null,"attachments":[]}';
+  const content = `Name: ${name}\n\nEmail: ${email}\n\nSubject: ${subject}\n\nMessage: ${message}`;
 
-  fetch(
-    'https://discord.com/api/webhooks/1342096332432736328/FBZdrk7651RKueOd8-qjgYWM5HCGxa2Oc1nCz5xLq3WRYweWzDKIwJOZZ6RAC-b-W51l',
-    {
-      headers: {
-        accept: 'application/json',
-        'accept-language': 'en',
-        'content-type': 'application/json',
-        'sec-ch-ua':
-          '"Google Chrome";v="107", "Chromium";v="107", "Not=A?Brand";v="24"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-      },
-      referrer: 'https://discohook.org/',
-      referrerPolicy: 'strict-origin',
-      body: formBody,
-      method: 'POST',
-      mode: 'cors',
-      credentials: 'omit',
-    }
-  )
+  const formBody = {
+    content,
+    embeds: null,
+    attachments: []
+  };
+
+ fetch("https://discord.com/api/v10/webhooks/1342096332432736328/FBZdrk7651RKueOd8-qjgYWM5HCGxa2Oc1nCz5xLq3WRYweWzDKIwJOZZ6RAC-b-W51l?wait=true", {
+  "headers": {
+    "accept": "application/json",
+    "accept-language": "en",
+    "content-type": "application/json",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site"
+  },
+  "referrer": "https://discohook.org/",
+  "referrerPolicy": "strict-origin",
+  "body": JSON.stringify(formBody),
+  "method": "POST",
+  "mode": "cors",
+  "credentials": "omit"
+})
     .then(() => {
       alert('Email sent successfully');
       form.reset();
